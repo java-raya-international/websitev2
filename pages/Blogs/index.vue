c8e8f16a6f89311bd84bf8cbd673182512c2da88
Nicko S.U. <nicko.su@hotmail.com>
2025-01-17 15:52:57 +0800

    FEATURES: Add for craft menu, Add product json skeleton

---
+++ pages/Blogs/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <LayoutTopHeader/>
+    <LayoutMiddleHeader/>
+    <LayoutNavbarStyleTwo/>
+
+
+    <LayoutFooter/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
